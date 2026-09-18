516285735657252555686b5ca706b507d2f538f7
steveaboy2-art <steveaboy2@gmail.com>
2026-09-18 11:25:18 +0530

    Configure Google Drive OAuth client ID

--- api/google-config.ts
+++ api/google-config.ts
@@ -1,4 +1,5 @@
+const CLIENT_ID = '14663212067-eb2lvc5qorg89vdgeod530ssaoaa2d1f.apps.googleusercontent.com';
+
 export default async function handler(req: Request) {
-  const clientId = process.env.GOOGLE_CLIENT_ID || '';
-  return Response.json({ clientId });
+  return Response.json({ clientId: CLIENT_ID });
 }
